a634ea90223e898f4ed4e4911f60a6e63306a26c
gabe-morris <gabe.e.morris@gmail.com>
2021-06-14 13:41:02 -0500

    finished tasks 1 & 2

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+    A: counter1 is receiving a closure from its parent function, while counter2 is receiving a variable that was declared before the function
   2. Which of the two uses a closure? How can you tell?
-  
+    A: Counter1 - its reaching into its parent scope to grab a value that has been defined
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
 */
@@ -62,8 +62,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.floor(Math.random()* 3); //Generates random score between 0 and 2
 }
 
 
@@ -81,10 +81,9 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inningCB,innings){
+  
 }
-
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
